77973f94466da56df6eef8b5911c5a1346f4c3f5
Pat Heard <patrick.heard@cds-snc.ca>
2022-01-18 22:08:31 +0000

    feat: Terraform for the S3 satellite ConfigRule
    Creates the S3 satellite ConfigRule and custom lambda to check
    the compliance of an account.
    
    Also moves all `config_rule` code into the `terraform/aws/config` module
    so that it can be easily shared across all accounts.

--- terragrunt/aws/config/iam.tf
+++ terragrunt/aws/config/iam.tf
@@ -5,8 +5,11 @@ resource "aws_iam_role" "security_config" {
 }
 
 resource "aws_iam_policy_attachment" "managed_policy" {
-  name       = "aws_config_managed_policy"
-  roles      = ["${aws_iam_role.security_config.name}"]
+  name = "aws_config_managed_policy"
+  roles = [
+    aws_iam_role.security_config.name,
+    aws_iam_role.cbs_s3_satellite_bucket_rule.name
+  ]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
 }
 
@@ -19,4 +22,16 @@ resource "aws_iam_policy_attachment" "aws-aws_config_policy-policy" {
   name       = "aws_config_policy"
   roles      = ["${aws_iam_role.security_config.name}"]
   policy_arn = aws_iam_policy.aws_config_policy.arn
+}
+
+data "aws_iam_policy_document" "lambda_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
 }
