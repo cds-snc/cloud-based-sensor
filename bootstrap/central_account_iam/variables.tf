5c221848ef2211ed30ce529cae459c277cfeb9a2
Mohamed Nur <mohamed.nur@cds-snc.ca>
2022-01-19 18:07:02 -0500

    docs: new account setup

--- bootstrap/central_account_iam/variables.tf
+++ bootstrap/central_account_iam/variables.tf
@@ -1,8 +1,3 @@
-variable "account_id" {
-  description = "(Required) The account ID to perform actions on."
-  type        = string
-}
-
 variable "region" {
   description = "The current AWS region"
   type        = string
