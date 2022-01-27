efec027ed7226b9b324b3971e853f32f2eaf14e5
Pat Heard <patrick.heard@cds-snc.ca>
2022-01-27 13:27:38 +0000

    feat: add AWS account ID to bootstrap script

--- bootstrap/satellite_account_iam/bootstrap.sh
+++ bootstrap/satellite_account_iam/bootstrap.sh
@@ -9,6 +9,8 @@ IFS=$'\n\t'
 #
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+ACCOUNT_ID="$(aws sts get-caller-identity | jq -r .Account)"
+echo -e "\n\033[0;33m⚡\033[0m Bootstrap satellite \033[0;35m$ACCOUNT_ID\033[0m\n"
 
 rm -rf "$SCRIPT_DIR"/terraform* "$SCRIPT_DIR"/.terraform*
 terraform -chdir="$SCRIPT_DIR" init
