c0b59bad6e246373d62fdb109316ccac9a8470f4
Pat Heard <patrick.heard@cds-snc.ca>
2022-01-28 08:45:03 -0500

    fix: set S3 logging buckets to correct module (#77)
    Update the two S3 access log buckets to use the correct module.
    This will grant them the expected `log-delivery-write` ACL.

--- terragrunt/aws/satellite_bucket/s3.tf
+++ terragrunt/aws/satellite_bucket/s3.tf
@@ -52,7 +52,7 @@ module "satellite_bucket" {
 }
 
 module "satellite_access_bucket" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v1.0.4//S3"
+  source            = "github.com/cds-snc/terraform-modules?ref=v1.0.4//S3_log_bucket"
   bucket_name       = "${var.satellite_bucket_name}-access"
   billing_tag_value = var.billing_tag_value
   force_destroy     = true
