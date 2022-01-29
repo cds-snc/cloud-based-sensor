239344af603814e8566e98c975c6e985272b3b98
Pat Heard <patrick.heard@cds-snc.ca>
2022-01-29 02:18:13 +0000

    fix: refactor satellite bucket policy
    The satellite bucket policy will also require policies items for
    log delivery from other AWS services.  To reflect this change, the
    policy definition is being moved out of the CloudTrail module into
    the satellite_bucket module.

--- terragrunt/aws/satellite_bucket/s3.tf
+++ terragrunt/aws/satellite_bucket/s3.tf
@@ -65,3 +65,167 @@ resource "aws_s3_bucket_ownership_controls" "satellite_bucket" {
     object_ownership = "ObjectWriter"
   }
 }
+
+#
+# Bucket policy allowing AWS services to write
+# to the satellite bucket
+#
+resource "aws_s3_bucket_policy" "satellite_bucket" {
+  bucket = module.satellite_bucket.s3_bucket_id
+  policy = data.aws_iam_policy_document.combined.json
+}
+
+data "aws_iam_policy_document" "combined" {
+  source_policy_documents = [
+    data.aws_iam_policy_document.cloudtrail_write_logs.json,
+    data.aws_iam_policy_document.log_delivery_write_logs.json,
+    data.aws_iam_policy_document.load_balancer_write_logs.json,
+    data.aws_iam_policy_document.deny_insecure_transport.json
+  ]
+}
+
+data "aws_iam_policy_document" "cloudtrail_write_logs" {
+
+  statement {
+    sid    = "CloudTrailGetAcl"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+    resources = [
+      module.satellite_bucket.s3_bucket_arn
+    ]
+  }
+
+  statement {
+    sid    = "CloudTrailPutObject"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "${module.satellite_bucket.s3_bucket_arn}/cloudtrail_logs/AWSLogs/${var.account_id}/*"
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:cloudtrail:${var.region}:${var.account_id}:trail/CbsSatelliteTrail"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "log_delivery_write_logs" {
+
+  statement {
+    sid    = "LogDeliveryGetAcl"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+    resources = [
+      module.satellite_bucket.s3_bucket_arn
+    ]
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${var.region}:${var.account_id}:*"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [var.account_id]
+    }
+  }
+
+  statement {
+    sid    = "LogDeliveryPutObject"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["delivery.logs.amazonaws.com"]
+    }
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "${module.satellite_bucket.s3_bucket_arn}/AWSLogs/${var.account_id}/*"
+    ]
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:logs:${var.region}:${var.account_id}:*"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [var.account_id]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+  }
+}
+
+data "aws_elb_service_account" "main" {}
+data "aws_iam_policy_document" "load_balancer_write_logs" {
+
+  statement {
+    sid    = "ELBLogDeliveryPutObject"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = data.aws_elb_service_account.main.arn
+    }
+    actions = [
+      "s3:PutObject",
+    ]
+    resources = [
+      "${module.satellite_bucket.s3_bucket_arn}/*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "deny_insecure_transport" {
+
+  statement {
+    sid    = "denyInsecureTransport"
+    effect = "Deny"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      module.satellite_bucket.s3_bucket_arn,
+      "${module.satellite_bucket.s3_bucket_arn}/*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values = [
+        "false"
+      ]
+    }
+  }
+}
