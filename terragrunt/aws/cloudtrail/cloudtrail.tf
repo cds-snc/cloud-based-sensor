d01c598f00fec25b61312e41c3039245fe12a241
Pat Heard <patrick.heard@cds-snc.ca>
2022-01-31 10:54:49 -0500

    fix: refactor satellite bucket policy (#82)
    The satellite bucket policy will also require policies items for
    log delivery from other AWS services.  To reflect this change, the
    policy definition is being moved out of the CloudTrail module into
    the satellite_bucket module.

--- terragrunt/aws/cloudtrail/cloudtrail.tf
+++ terragrunt/aws/cloudtrail/cloudtrail.tf
@@ -1,9 +1,3 @@
-locals {
-  satellite_bucket_arn = "arn:aws:s3:::${var.satellite_bucket_name}"
-  trail_prefix         = "cloudtrail_logs"
-  trail_arn            = "arn:aws:cloudtrail:${var.region}:${var.account_id}:trail/CbsSatelliteTrail"
-}
-
 #
 # CloudTrail: deliver all events from all regions
 # to the S3 satellite bucket
@@ -11,62 +5,12 @@ locals {
 resource "aws_cloudtrail" "satellite_trail" {
   name                          = "CbsSatelliteTrail"
   s3_bucket_name                = var.satellite_bucket_name
-  s3_key_prefix                 = local.trail_prefix
+  s3_key_prefix                 = "cloudtrail_logs"
   include_global_service_events = true
   is_multi_region_trail         = true
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
     Terraform             = true
-  }
-
-  depends_on = [
-    aws_s3_bucket_policy.satellite_trail
-  ]
-}
-
-#
-# Bucket policy allowing CloudTrail to write logs
-# to the satellite bucket
-#
-resource "aws_s3_bucket_policy" "satellite_trail" {
-  bucket = var.satellite_bucket_name
-  policy = data.aws_iam_policy_document.satellite_trail.json
-}
-
-data "aws_iam_policy_document" "satellite_trail" {
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-    actions = [
-      "s3:GetBucketAcl"
-    ]
-    resources = [
-      local.satellite_bucket_arn
-    ]
-  }
-  statement {
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-    actions = [
-      "s3:PutObject"
-    ]
-    resources = [
-      "${local.satellite_bucket_arn}/${local.trail_prefix}/AWSLogs/${var.account_id}/*"
-    ]
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values   = ["bucket-owner-full-control"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceArn"
-      values   = [local.trail_arn]
-    }
   }
 }
