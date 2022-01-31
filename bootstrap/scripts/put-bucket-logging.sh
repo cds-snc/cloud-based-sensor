abd55ebd229e6b206c649a002cf71ddbdbdb0e09
Pat Heard <patrick.heard@cds-snc.ca>
2022-01-31 14:33:03 -0500

    feat: add put-bucket-logging script (#87)
    Script that can be used to set S3 buckets in an account to
    use the satellite bucket as the access log destination.
    
    This fixes compliance of the `cbs_s3_bucket_logging_enabled`
    ConfigRule.

---
+++ bootstrap/scripts/put-bucket-logging.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# PURPOSE:
+# Sets the given target bucket(s) to use the satellite bucket 
+# in the account as the access log destination.
+#
+# USE:
+# ./put-bucket-logging.sh 'bucket-name-1 bucket-name-2 bucket-name-3'
+#
+
+set -euo pipefail
+IFS=$' '
+
+BUCKET_NAMES="$1"
+ACCOUNT_ID="$(aws sts get-caller-identity | jq -r .Account)"
+SATELLITE_BUCKET_NAME="cbs-satellite-${ACCOUNT_ID}"
+LOG_PREFIX="s3_access_logs"
+
+echo -e "\n🪣  S3 access logging for \033[0;35m${ACCOUNT_ID}\033[0m\n"
+
+for BUCKET in ${BUCKET_NAMES}; do
+    echo -e "Bucket \033[0;33m${BUCKET}\033[0m"
+    aws s3api put-bucket-logging \
+        --bucket "${BUCKET}" \
+        --bucket-logging-status '{"LoggingEnabled":{"TargetBucket":"'"${SATELLITE_BUCKET_NAME}"'","TargetPrefix":"'"${LOG_PREFIX}"'/"}}'    
+done
