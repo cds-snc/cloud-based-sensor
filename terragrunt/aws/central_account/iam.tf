1ef59050dd5af0c5cd1aa48d3f94806b5e778486
Pat Heard <patrick.heard@cds-snc.ca>
2022-02-03 20:07:27 +0000

    feat: rename log archive read role

--- terragrunt/aws/central_account/iam.tf
+++ terragrunt/aws/central_account/iam.tf
@@ -2,7 +2,7 @@
 # Role used to read objects from the log archive bucket
 #
 resource "aws_iam_role" "log_archive_read" {
-  name               = "CbsLogArchiveBucketRead"
+  name               = "CbsASEAReaderRole"
   assume_role_policy = sensitive(data.aws_iam_policy_document.log_archive_read_assume.json)
 }
 
