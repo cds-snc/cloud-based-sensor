b0b2a308cf704e6e330a55fdcf898fe0a608ef90
Pat Heard <patrick.heard@cds-snc.ca>
2022-02-03 21:04:37 +0000

    fix: bucket name and add event ID

--- terragrunt/aws/central_account/s3.tf
+++ terragrunt/aws/central_account/s3.tf
@@ -88,9 +88,10 @@ data "aws_lambda_function" "cbs_transport_lambda" {
 }
 
 resource "aws_s3_bucket_notification" "cbs_transport_lambda" {
-  bucket = aws_s3_bucket.log_archive_bucket.id
+  bucket = module.log_archive_bucket.s3_bucket_id
 
   lambda_function {
+    id                  = "CbsEvent"
     lambda_function_arn = data.aws_lambda_function.cbs_transport_lambda.arn
     events              = ["s3:ObjectCreated:*"]
   }
