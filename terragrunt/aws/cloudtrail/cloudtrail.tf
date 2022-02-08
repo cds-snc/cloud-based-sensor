5734804df994d484d3282c65e082287eddd673c3
Pat Heard <patrick.heard@cds-snc.ca>
2022-02-08 11:16:36 -0500

    feat: add CloudTrail to central account (#117)
    This will send logs directly to the log-archive bucket and be
    used to watch for anomalies in the account.

--- terragrunt/aws/cloudtrail/cloudtrail.tf
+++ terragrunt/aws/cloudtrail/cloudtrail.tf
@@ -4,7 +4,7 @@
 #
 resource "aws_cloudtrail" "satellite_trail" {
   name                          = "CbsSatelliteTrail"
-  s3_bucket_name                = var.satellite_bucket_name
+  s3_bucket_name                = var.cloudtrail_bucket_name
   s3_key_prefix                 = "cloudtrail_logs"
   include_global_service_events = true
   is_multi_region_trail         = true
