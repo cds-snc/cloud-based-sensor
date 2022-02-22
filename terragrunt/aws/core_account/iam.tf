86841c75d5248287cd33eb9c333064d5b3253c73
Cheyenne Arrowsmith <cheyenne.arrowsmith@cds-snc.ca>
2022-02-22 13:57:04 -0500

    fix: fixed variable assingment

--- terragrunt/aws/core_account/iam.tf
+++ terragrunt/aws/core_account/iam.tf
@@ -3,8 +3,24 @@
 # of the `bootstrap/satellite_account_iam`.
 #
 
+resource "aws_iam_role" "s3_replicate" {
+  name               = "CoreReplicateToCBSCentral"
+  assume_role_policy = data.aws_iam_policy_document.s3_replicate_assume.json
+}
+
+data "aws_iam_policy_document" "s3_replicate_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role_policy_attachment" "s3_replicate" {
-  role       = data.aws_iam_role.s3_replicate.name
+  role       = aws_iam_role.s3_replicate.name
   policy_arn = aws_iam_policy.s3_replicate.arn
 }
 
@@ -39,21 +55,5 @@ data "aws_iam_policy_document" "s3_replicate" {
     resources = [
       "${local.log_archive_bucket_arn}/*",
     ]
-  }
-}
-
-resource "aws_iam_role" "s3_replicate" {
-  name               = "CoreReplicateToCBSCentral"
-  assume_role_policy = data.aws_iam_policy_document.s3_replicate_assume.json
-}
-
-data "aws_iam_policy_document" "s3_replicate_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    effect  = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
   }
 }
