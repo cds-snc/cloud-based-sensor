832aea2f8feea67860d2787463689df016485ed8
Pat Heard <patrick.heard@cds-snc.ca>
2022-02-24 15:46:36 +0000

    fix: add `cbs` suffix to prevent name clashes

--- terragrunt/aws/alarms/sns.tf
+++ terragrunt/aws/alarms/sns.tf
@@ -4,7 +4,7 @@
 resource "aws_sns_topic" "cloudwatch_alarm_us_east" {
   provider = aws.us-east-1
 
-  name              = "cloudwatch_alarm"
+  name              = "cloudwatch_alarm_cbs"
   kms_master_key_id = aws_kms_key.sns_cloudwatch_us_east.id
 
   tags = {
@@ -24,7 +24,7 @@ resource "aws_sns_topic_subscription" "cloudwatch_alarm_us_east" {
 module "notify_slack" {
   source = "github.com/cds-snc/terraform-modules?ref=v1.0.10//notify_slack"
 
-  function_name     = "notify_slack"
+  function_name     = "notify_slack_cbs"
   project_name      = var.account_id
   slack_webhook_url = var.slack_webhook_url
   sns_topic_arns    = [aws_sns_topic.cloudwatch_alarm_us_east.arn]
