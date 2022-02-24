10bb572d477197bd3874532bfd364de1cb496d05
Pat Heard <patrick.heard@cds-snc.ca>
2022-02-24 11:28:29 -0500

    feat: CloudWatch alarm for billing changes (#126)
    Add a CloudWatch alarm, SNS topic and Slack notification Lambda
    for estimated billing changes that are greater than a percentage
    change threshold in a six hour period.
    
    Note that billing alerts must be enabled on the payer account
    for the `EstimatedCharges` metric to be available in CloudWatch.

---
+++ terragrunt/aws/alarms/billing_alarm.tf
@@ -0,0 +1,47 @@
+# EstimatedCharges metrics are published every ~6 hours
+resource "aws_cloudwatch_metric_alarm" "billing_change_over_threshold" {
+  provider = aws.us-east-1
+
+  alarm_name          = "BillingChangeOverThreshold"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  threshold           = var.billing_change_percent_threshold
+  alarm_description   = "Estimated billing change greater than threshold in 6 hour period"
+  treat_missing_data  = "notBreaching"
+  alarm_actions       = [aws_sns_topic.cloudwatch_alarm_us_east.arn]
+  ok_actions          = [aws_sns_topic.cloudwatch_alarm_us_east.arn]
+
+  metric_query {
+    id    = "current"
+    label = "Current charges"
+
+    metric {
+      metric_name = "EstimatedCharges"
+      namespace   = "AWS/Billing"
+      period      = "21600"
+      stat        = "Maximum"
+      dimensions = {
+        Currency = "USD"
+      }
+    }
+  }
+
+  metric_query {
+    id         = "delta"
+    expression = "RATE(current) * PERIOD(current)"
+    label      = "Delta"
+  }
+
+  metric_query {
+    id         = "previous"
+    expression = "current - delta"
+    label      = "Previous charges"
+  }
+
+  metric_query {
+    id          = "percent_change"
+    expression  = "ABS(100 * delta/previous)"
+    label       = "Percent change"
+    return_data = "true"
+  }
+}
---
+++ terragrunt/aws/alarms/sns.tf
@@ -0,0 +1,34 @@
+#
+# SNS: topic, subscription and Lambda that posts to Slack
+#
+resource "aws_sns_topic" "cloudwatch_alarm_us_east" {
+  provider = aws.us-east-1
+
+  name              = "cloudwatch_alarm_cbs"
+  kms_master_key_id = aws_kms_key.sns_cloudwatch_us_east.id
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+resource "aws_sns_topic_subscription" "cloudwatch_alarm_us_east" {
+  provider = aws.us-east-1
+
+  topic_arn = aws_sns_topic.cloudwatch_alarm_us_east.arn
+  protocol  = "lambda"
+  endpoint  = module.notify_slack.lambda_arn
+}
+
+module "notify_slack" {
+  source = "github.com/cds-snc/terraform-modules?ref=v1.0.10//notify_slack"
+
+  function_name     = "notify_slack_cbs"
+  project_name      = var.account_id
+  slack_webhook_url = var.slack_webhook_url
+  sns_topic_arns    = [aws_sns_topic.cloudwatch_alarm_us_east.arn]
+
+  billing_tag_key   = var.billing_tag_key
+  billing_tag_value = var.billing_tag_value
+}
