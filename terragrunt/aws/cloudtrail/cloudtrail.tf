ab24a67806c5d1f3bcc2717eea834edb3f1e543c
Cheyenne Arrowsmith <cheyenne.arrowsmith@cds-snc.ca>
2022-03-24 18:23:53 +0000

    Removal of cloudtrail.tf to kick off cloudtrail removal - The Sphinx

--- terragrunt/aws/cloudtrail/cloudtrail.tf
+++ terragrunt/aws/cloudtrail/cloudtrail.tf
@@ -1,16 +1,1 @@
-#
-# CloudTrail: deliver all events from all regions
-# to the S3 satellite bucket
-#
-resource "aws_cloudtrail" "satellite_trail" {
-  name                          = "CbsSatelliteTrail"
-  s3_bucket_name                = var.cloudtrail_bucket_name
-  s3_key_prefix                 = "cloudtrail_logs"
-  include_global_service_events = true
-  is_multi_region_trail         = true
 
-  tags = {
-    (var.billing_tag_key) = var.billing_tag_value
-    Terraform             = true
-  }
-}
