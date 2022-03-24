0c6f70bcd258afc57ef90590bc89df1010b6d80c
Cheyenne Arrowsmith <cheyenne.arrowsmith@cds-snc.ca>
2022-03-24 19:09:01 +0000

    remove all traces of cloudtrail

--- terragrunt/aws/cloudtrail/cloudtrail.tf
+++
@@ -1,1 +0,0 @@
-# Removing cloudtrail from EVERYTHING
--- terragrunt/aws/cloudtrail/inputs.tf
+++
@@ -1,4 +0,0 @@
-variable "cloudtrail_bucket_name" {
-  description = "Name of the S3 bucket that CloudTrail will send logs to"
-  type        = string
-}
