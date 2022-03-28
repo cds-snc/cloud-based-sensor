3316260b1b8bbacfab8a406ae5aca3ae4d9f4b45
Pat Heard <patrick.heard@cds-snc.ca>
2022-03-28 15:34:45 +0000

    chore: update variable description

--- terragrunt/aws/alarms/inputs.tf
+++ terragrunt/aws/alarms/inputs.tf
@@ -1,5 +1,5 @@
 variable "slack_webhook_url" {
-  description = "Slack webhook used by the Notify Slack Lambda function."
+  description = "Slack webhook used by the Notify Slack Lambda function when an alarm triggers."
   type        = string
   sensitive   = true
 }
