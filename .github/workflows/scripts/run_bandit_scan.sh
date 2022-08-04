6d447c0563020568870e5aca853319de3f2fbfea
CalvinRodo <calvin.rodo@cds-snc.ca>
2022-08-04 16:55:13 -0400

    feat: remove config

--- .github/workflows/scripts/run_bandit_scan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-# Make sure we are using the latest version
-docker pull cytopia/bandit:latest
-
-# Scan source code and only report on high severity issues
-docker run --rm -v "$(pwd)":/data cytopia/bandit -r /data -ll
--- terragrunt/env/satellite/config/terragrunt.hcl
+++ terragrunt/env/satellite/config/terragrunt.hcl
@@ -2,10 +2,6 @@ terraform {
   source = "../../../aws//config"
 }
 
-inputs = {
-  config_max_execution_frequency = "TwentyFour_Hours"
-}
-
 include {
   path = find_in_parent_folders()
   expose = true
