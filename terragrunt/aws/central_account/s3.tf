6cac3c34c0afd0fa2df5dcf0454c69f788d6f088
Pat Heard <patrick.heard@cds-snc.ca>
2022-10-28 08:49:45 -0400

    fix: create SNS topic for transport lambda (#177)
    Add an SNS topic to the central log archive account as this is now
    the mechanism CBS uses to invoke the transport lambda.

--- terragrunt/aws/central_account/s3.tf
+++ terragrunt/aws/central_account/s3.tf
@@ -99,18 +99,14 @@ data "aws_iam_policy_document" "log_archive_bucket" {
 }
 
 #
-# Notify the CBS transport Lambda when a new object is created
+# Publish a notification to the SNS topic when objects are created
 #
-data "aws_lambda_function" "cbs_transport_lambda" {
-  function_name = var.cbs_transport_lambda_name
-}
-
 resource "aws_s3_bucket_notification" "cbs_transport_lambda" {
   bucket = module.log_archive_bucket.s3_bucket_id
 
-  lambda_function {
-    id                  = "CbsEvent"
-    lambda_function_arn = data.aws_lambda_function.cbs_transport_lambda.arn
-    events              = ["s3:ObjectCreated:*"]
+  topic {
+    id        = "CbsEvent"
+    topic_arn = aws_sns_topic.log_archive.arn
+    events    = ["s3:ObjectCreated:*"]
   }
 }
---
+++ terragrunt/aws/central_account/sns.tf
@@ -0,0 +1,87 @@
+resource "aws_sns_topic" "log_archive" {
+  name              = "log-archive"
+  kms_master_key_id = aws_kms_key.log_archive_encrypt.id
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+  }
+}
+
+resource "aws_sns_topic_policy" "log_archive" {
+  arn    = aws_sns_topic.log_archive.arn
+  policy = data.aws_iam_policy_document.log_archive_topic_policy.json
+}
+
+data "aws_iam_policy_document" "log_archive_topic_policy" {
+  policy_id = "SNS Access Policy"
+
+  # Allow this account to use the topic
+  statement {
+    sid    = "AllowSelfAccess"
+    effect = "Allow"
+    actions = [
+      "SNS:Publish",
+      "SNS:RemovePermission",
+      "SNS:SetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:AddPermission",
+      "SNS:Subscribe"
+    ]
+    resources = [
+      aws_sns_topic.log_archive.arn
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceOwner"
+      values = [
+        var.log_archive_account_id,
+      ]
+    }
+  }
+
+  # Allow transport Lambda to consume SNS notifications
+  statement {
+    sid    = "AllowLambdaAccess"
+    effect = "Allow"
+    actions = [
+      "SNS:Subscribe",
+      "SNS:ListSubscriptionsByTopic"
+    ]
+    resources = [
+      aws_sns_topic.log_archive.arn
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.log_archive_account_id}:root"]
+    }
+  }
+
+  # Allow S3 log archive bucket to publish SNS notifications
+  statement {
+    sid    = "AllowS3Access"
+    effect = "Allow"
+    actions = [
+      "SNS:Publish"
+    ]
+    resources = [
+      aws_sns_topic.log_archive.arn
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values = [
+        module.log_archive_bucket.s3_bucket_arn
+      ]
+    }
+  }
+}
