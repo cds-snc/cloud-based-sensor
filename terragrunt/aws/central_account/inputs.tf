cc38064ac5265ba66e8c237985897f43855c13e8
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2024-06-03 11:46:13 -0400

    Feat/setup roles cbs next (#345)
    * feat: create new role for CBS principal
    
    * feat: update workflows to access new role
    
    * chore: run fmt
    
    * fix: typo
    
    * fix: mark identifiers as sensitive
    
    * fix: pass set of string
    
    * fix: same typo

--- terragrunt/aws/central_account/inputs.tf
+++ terragrunt/aws/central_account/inputs.tf
@@ -4,6 +4,12 @@ variable "cbs_principal_arn" {
   sensitive   = true
 }
 
+variable "cbs_principal_role_arn" {
+  description = "ARN of the CBS principal role that reads objects from the log archive bucket (used upon decommissioning of CBS v1.0)"
+  type        = string
+  sensitive   = true
+}
+
 variable "cbs_transport_lambda_name" {
   description = "Name of the CBS transport Lambda function"
   type        = string
